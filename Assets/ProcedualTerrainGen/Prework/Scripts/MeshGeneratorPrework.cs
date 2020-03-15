a0e006234e2d061c5c383c73a2e92c7cfdafc59a
drumath <2237k.drumath@gmail.com>
2020-03-15 21:37:35 +0900

    :sparkles: 中心座標が間違っていたので変更

--- Assets/ProcedualTerrainGen/Prework/Scripts/MeshGeneratorPrework.cs
+++ Assets/ProcedualTerrainGen/Prework/Scripts/MeshGeneratorPrework.cs
@@ -20,36 +20,11 @@ namespace ProcedualTerrainGen.Prework
             {
                 for (var j = 0; j <= xSize; j++)
                 {
-                    vertices.Add(new Vector3(i- (xSize/2f), 0, j-(zSize/2f)));
+                    vertices.Add(new Vector3(i- (zSize/2f), 0, j-(xSize/2f)));
                 }
             }
             mesh.SetVertices(vertices);
 
-//            int[] tris = new int[xSize * 6 * zSize];
-//            for (int i = 0; i <= zSize; i++)
-//            {
-//                for (int j = 0; j < xSize; j++)
-//                {
-//                    var offset = (i * (xSize + 1) + j) * 3 * 2;
-//                    int CalcVerticesIndex(int _i, int _j) => _i * (xSize + 1) + _j;
-//
-//                    tris[offset + 0] = CalcVerticesIndex(i, j);
-//                    tris[offset + 1] = CalcVerticesIndex(i, j + 1);
-//                    tris[offset + 2] = CalcVerticesIndex(i + 1, j);
-//                    
-//                    tris[offset + 3] = CalcVerticesIndex(i, j + 1);
-//                    tris[offset + 4] = CalcVerticesIndex(i + 1, j + 1);
-//                    tris[offset + 5] = CalcVerticesIndex(i + 1, j);
-//
-//                }
-//            }
-
-//            int[] tris = new[]
-//            {
-//                0, 1, 6,
-//                7,6,1
-//            };
-//            mesh.triangles = tris;
 
             List<int> triangles = new List<int>();
             for(int i=0; i<zSize; i++)
