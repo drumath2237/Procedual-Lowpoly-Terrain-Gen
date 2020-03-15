ed12684e850d69b4589dd5164f8f663ec873c601
drumath <2237k.drumath@gmail.com>
2020-03-15 20:59:23 +0900

    :bug: なんかIndexOutOfRangeエラー出ちゃってる...
    文献漁ってたらMesh.SetVerticesやSetTrianglesで頂点とかリストで設定できるらしいのでそっちに切り替える

--- Assets/ProcedualTerrainGen/Prework/Scripts/MeshGeneratorPrework.cs
+++ Assets/ProcedualTerrainGen/Prework/Scripts/MeshGeneratorPrework.cs
@@ -15,9 +15,9 @@ namespace ProcedualTerrainGen.Prework
             Mesh mesh = new Mesh();
 
             Vector3[] vecs = new Vector3[(xSize + 1) * (zSize + 1)];
-            for(var i=0; i<=xSize; i++)
+            for(var i=0; i<=zSize; i++)
             {
-                for (var j = 0; j <= zSize; j++)
+                for (var j = 0; j <= xSize; j++)
                 {
                     Vector3 vec = new Vector3(i- (xSize/2f), 0, j-(zSize/2f));
                     vecs[i * (xSize + 1) + j] = vec;
@@ -25,6 +25,32 @@ namespace ProcedualTerrainGen.Prework
                 
             }
             mesh.vertices = vecs;
+
+//            int[] tris = new int[xSize * 6 * zSize];
+//            for (int i = 0; i <= zSize; i++)
+//            {
+//                for (int j = 0; j < xSize; j++)
+//                {
+//                    var offset = (i * (xSize + 1) + j) * 3 * 2;
+//                    int CalcVerticesIndex(int _i, int _j) => _i * (xSize + 1) + _j;
+//
+//                    tris[offset + 0] = CalcVerticesIndex(i, j);
+//                    tris[offset + 1] = CalcVerticesIndex(i, j + 1);
+//                    tris[offset + 2] = CalcVerticesIndex(i + 1, j);
+//                    
+//                    tris[offset + 3] = CalcVerticesIndex(i, j + 1);
+//                    tris[offset + 4] = CalcVerticesIndex(i + 1, j + 1);
+//                    tris[offset + 5] = CalcVerticesIndex(i + 1, j);
+//
+//                }
+//            }
+
+            int[] tris = new[]
+            {
+                0, 1, 6,
+                7,6,1
+            };
+            mesh.triangles = tris;
 
 
             mesh.RecalculateNormals();
